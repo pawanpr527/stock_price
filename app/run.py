62daeec2cbc583f5c8d8ed56479007d3cb8524e7
Pawan Prajapati <pawanprajapati@Govinds-Air.bbrouter>
2025-08-04 21:07:08 +0530

    DashBoard Building

--- app/run.py
+++ app/run.py
@@ -1,19 +1,49 @@
 from flask import Flask,redirect,render_template,request,url_for
+import redis
 app = Flask(__name__)
+app.secret_key = "pawan236007"
+
+r = redis.Redis(host='localhost',port=6379,db=0,decode_responses=True)
+
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('login.html')
 
-@app.route('/login', methods=['POST','GET'])
+
+@app.route('/registration',methods=['GET','POST'])
+def register():
+  if request.method=="POST":  
+    name = request.form.get('First')
+    last = request.form.get('Last')
+    email = request.form.get('Email')
+    password = request.form.get('Password')
+    r.hset(f"user:{email}",
+      mapping={
+          'first' : name,
+          'last' : last,
+          'password' : password,
+          'email' : email
+      }
+    )
+    return redirect(url_for('index'))
+  return render_template('registration.html')
+
+
+@app.route('/login', methods=['POST'])
 def login():
     email = request.form.get('email')
     password = request.form.get('password')
-    
     # Dummy check (you can add real auth here)
-    if email == "admin@example.com" and password == "1234":
-        return redirect(url_for('dashboard'))
+    user_key = f"user:{email}"
+    if r.exists(user_key):
+       storedpass = r.hget(user_key,'password')
+       if password==storedpass:
+          return render_template('dashboard.html')
+       else:
+          return "Incorrect password"
     else:
-        return "Invalid credentials", 401
+       return "Email not registered"
+
 if __name__=="__main__":
     app.run(debug=True)
